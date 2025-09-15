e41de3bc23d14acc641cfdf5f175cbbcd444113f
Kalana <kalana33471@gmail.com>
2025-09-15 19:47:11 +0530

    added cards to the home page

--- asgardeo-react/src/data/mock.js
+++ asgardeo-react/src/data/mock.js
@@ -1,4 +1,10 @@
 export const playlists = [
   { id: 1, title: "Chill Vibes", cover: "https://picsum.photos/200?random=1" },
-  { id: 2, title: "Workout Mix", cover: "https://picsum.photos/200?random=2" }
+  { id: 2, title: "Workout Mix", cover: "https://picsum.photos/200?random=2" },
+  { id: 3, title: "Pop", cover: "https://picsum.photos/200?random=3" },
+  { id: 4, title: "Hollywood", cover: "https://picsum.photos/200?random=4" },
+  { id: 5, title: "Old Classic", cover: "https://picsum.photos/200?random=5" },
+  { id: 6, title: "Rap", cover: "https://picsum.photos/200?random=6" },
+  { id: 7, title: "Old School", cover: "https://picsum.photos/200?random=7" },
+  { id: 8, title: "Indian", cover: "https://picsum.photos/200?random=8" }
 ];
--- asgardeo-react/src/pages/Home.jsx
+++ asgardeo-react/src/pages/Home.jsx
@@ -2,8 +2,13 @@ import { playlists } from "../data/mock";
 
 export default function Home () {
     return (
-        <div className="bg-background">
-            <h1>Home</h1>
+        <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
+            {playlists.map((item) => (
+                 <div key={item.id} className="bg-neutral-800 p-4 rounded hover:bg-neutral-700 transition">
+                    <img src={item.cover} alt={item.title} className="w-full rounded mb-2" />
+                    <h3 className="text-text font-semibold">{item.title}</h3>
+                </div>
+            ))}
         </div>
     )
 }
