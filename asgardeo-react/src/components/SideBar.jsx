10bcf96fd7f51bfc2f6afdd61aa76374ddb17695
Kalana <kalana33471@gmail.com>
2025-09-15 17:02:06 +0530

    fixing

--- asgardeo-react/src/components/SideBar.jsx
+++ asgardeo-react/src/components/SideBar.jsx
@@ -1,11 +1,30 @@
+import { Home, Library, Search, User } from "lucide-react"
+import { NavLink } from "react-router-dom"
+
 export default function SideBar () {
+
+    const navItems = [
+        { icon: Home, label: "Home", path: "/"},
+        { icon: Search, label: "Search", path: "/search"},
+        { icon: Library, label: "Library", path: "/library"},
+        { icon: User, label: "User", path: "/profile"}
+    ]
+
     return (
         <aside className="bg-black text-white p-6 w-60">
-            <h1 className="text-xl font-bold mb-6">My Spotify</h1>
-            <nav className="space-y-3">
-                <div>Home</div>
-                <div>Search</div>
-                <div>Library</div>
+            <div>
+                <h1 className="text-xl font-bold mb-6">My Spotify</h1>
+            </div>
+            <nav>
+                {navItems.map((item) => (
+                    <NavLink
+                        key={item.path}
+                        to={item.path}
+                    >
+                        <item.icon size={20}/>
+                        <span>{item.label}</span>
+                    </NavLink>
+                ))}
             </nav>
         </aside>
     );
